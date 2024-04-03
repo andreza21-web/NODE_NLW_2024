32ad77bab36eca1fc2c8e314fb39785fd255efe5
Andreza Pessoa <andreza.21pessoa@gmail.com>
2024-04-03 20:44:41 -0300

    feat: criando rota post events

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,35 @@
 import fastify from 'fastify';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
-// METODOS HTTP POST, GET, PUT, DELETE
-// CORPO DA REQUEST
-// PARAMETROS DE BUSCA
-// PARAMETRO DE ROTA
-// CABEÇALHO
 
-app.get('/', () => {
-    return "NLW UNITE"
+const prisma = new PrismaClient({
+    log: ['query'],
 });
 
-// CONFIGURAÇÃO DA ROTA 36:35
+
+app.post('/events', async (request, reply) => {
+
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    })
+    const data = createEventSchema.parse(request.body);
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        },
+    })
+
+    return reply.status(201).send({ eventId: event.id })
+});
+
 app.listen({ port: 3333 }).then(() => {
     console.log('listening on port');
 });

8c03da41091cf2ffc062042fb75241a3ce74a290
Andreza Pessoa <andreza.21pessoa@gmail.com>
2024-04-03 19:19:37 -0300

    feature: criando a primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
-console.log("hello word!node")
-console.log("")
+import fastify from 'fastify';
+
+const app = fastify();
+// METODOS HTTP POST, GET, PUT, DELETE
+// CORPO DA REQUEST
+// PARAMETROS DE BUSCA
+// PARAMETRO DE ROTA
+// CABEÇALHO
+
+app.get('/', () => {
+    return "NLW UNITE"
+});
+
+// CONFIGURAÇÃO DA ROTA 36:35
+app.listen({ port: 3333 }).then(() => {
+    console.log('listening on port');
+});
